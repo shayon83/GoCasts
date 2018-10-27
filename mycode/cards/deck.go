f0f8d709846ef870cafc2c9b3dcacb78b86f4022
Sayan Bhattacharya <shayon83@gmail.com>
2018-10-26 23:00:42 -0700

    Cards

--- mycode/cards/deck.go
+++ mycode/cards/deck.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
 )
 
 // Create a new type of 'deck'
@@ -34,5 +37,21 @@ func deal(d deck, handSize int) (deck, deck) {
 }
 
 func (d deck) toString() string {
-	[]string(d).Join(",")
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		// Option #1 - Log the error and return a call to newDeck()
+		// Option #2 - Log the error and entirely quit the program
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	s := strings.Split(string(bs), ",")
+	return deck(s)
 }
--- mycode/cards/main.go
+++ mycode/cards/main.go
@@ -3,11 +3,6 @@ package main
 var deckType string
 
 func main() {
-	cards := newDeck()
-
-	hand, remainingDeck := deal(cards, 4)
-
-	hand.print()
-	remainingDeck.print()
-
+	cards := newDeckFromFile("mycards")
+	cards.print()
 }
